b6887d932c757cd78d8b937ed4b545b1cc0bb8a2
kerlynn <43892411+kerlynn@users.noreply.github.com>
2022-06-04 19:06:11 +0800

    Add files via upload

---
+++ start_work.py
@@ -0,0 +1,17 @@
+import webbrowser
+import os
+
+
+# Check Email
+webbrowser.open('https://mail.google.com/mail/u/0/')
+
+# Check Task
+webbrowser.open('http://jira-easystore.atlassian.net')
+
+# Check Pending PR
+webbrowser.open('https://bitbucket.org/')
+
+# Check Issue
+webbrowser.open('https://sentry.io/')
+
+os.system("open -a slack")
